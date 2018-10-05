106a514575e4ff934ac56c0ba379a3216d951bc3
Akhilesh Soni <akhileshsoni96@gmail.com>
2018-10-05 14:21:14 +0530

    Updated modularexpo

--- NumberTheory/modularexp.cpp
+++ NumberTheory/modularexp.cpp
@@ -1,17 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll ;
-ll exp(ll x,ll n,ll m)
-{
-	if(n==0)
-		return 1;
-	else if(n==1)
-		return(x%m);
-	else if(n%2==0)
-		return(exp((x%m*x%m)%m,n/2,m)%m);
-	else
-		return((x%m*exp((x%m*x%m)%m,(n-1)/2,m)%m)%m);
-}
+
+ll exp(ll x,ll y,ll p) 
+{ 
+    ll res = 1;
+    x = x % p; 
+    while (y > 0) 
+    { 
+        if (y & 1) 
+            res = (res*x) % p; 
+        y = y>>1; 
+        x = (x*x) % p;   
+    } 
+    return res; 
+} 
+
 int main()
 {
 	ios::sync_with_stdio(false);
