87f36138c0a5a1ca3218a57a399781e2ffe2d4af
Akhilesh Soni <akhileshsoni96@gmail.com>
2021-07-11 01:02:05 +0530

    Update dijkstra.cpp

--- Graphs/dijkstra.cpp
+++ Graphs/dijkstra.cpp
@@ -1,87 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-typedef pair<ll,ll> pii;
 
-#define size 100000
+typedef pair<int, int> pii;
+
 #define INF INT_MAX
 
-ll dis[size],par[size];
-bool vis[size];
+class Solution {
+    public:
+    void djikstra(const vector< list<pii> >&G, const int src, vector<int>&dis, vector<bool>&vis) {
+        priority_queue< pii, vector<pii>, greater<pii> >Q;
+        Q.push(make_pair(0,src));
+        dis[src] = 0;
+        while(!Q.empty()) {
+            int u;
+            u = Q.top().second;
+            Q.pop();
+            if(vis[u])
+                continue;
+            vis[u] = true;
+            for(auto it : G[u]) {
+                int v = it.second;
+                int w = it.first;
+                if(!vis[v] && dis[v] > dis[u]+w) {
+                    dis[v] = dis[u] + w;
+                    Q.push(make_pair(dis[v],v));
+                }
+            }
+        }
+    }
+};
 
-void dijkstra(vector< list< pii > >&G,ll s)
-{
-	ll i;
-	priority_queue< pii, vector<pii> , greater<pii> > Q; //Min Heap
-	Q.push(make_pair(0,s));
-	dis[s]=0;
-	while(!Q.empty())
-	{
-		ll u,v,w;
-		u=Q.top().second;
-		Q.pop();
-		if(vis[u])
-			continue;
-		vis[u]=1;
-		for(auto it=G[u].begin();it!=G[u].end();it++)
-		{
-			v=(*it).second;
-			w=(*it).first;
-			if(!vis[v] && dis[v]>dis[u]+w)
-			{
-				dis[v]=dis[u]+w;
-				par[v]=u;
-				Q.push(make_pair(dis[v],v));
-			}
-		}
-	}
-}
-
-void path(ll u,ll v)
-{
-	ll i;
-	i=v;
-	while(i!=u)
-	{
-		cout << i << "<--";
-		i=par[i];
-	}
-	cout << u << "\n";
-}
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	ll i,V,E;
-	cin >> V >> E;
-	vector< list< pii > >G(V);
-	for(i=0;i<V;i++)
-	{
-		G[i].clear();
-		vis[i]=0;
-		dis[i]=INF;
-	}
-	for(i=0;i<E;i++)
-	{
-		ll m,n,w;
-		cin >> m >> n >> w;
-		G[m].push_back(make_pair(w,n));
-		G[n].push_back(make_pair(w,m));
-	}
-	ll s;
-	cin >> s;
-	dijkstra(G,s);
-	for(i=0;i<V;i++)
-	{
-		if(dis[i]>=INF)
-			cout << "-1 ";
-		else if(i==s)
-			continue;
-		else
-			printf("dis[%lld]:%lld " ,i,dis[i]);
-	}
-	ll u,v;
-	cin >> u >> v;
-	path(u,v);
-	return 0;
+int main() {
+    ios::sync_with_stdio(0);
+    int V, E;
+    cin >> V >> E;
+    vector< list<pii> >G(V);
+    vector<bool> vis(V);
+    vector<int> dis(V);
+    for(int i=0; i<V; i++) {
+        G[i].clear();
+        vis[i] = false;
+        dis[i] = INF;
+    }
+    
+    for(int i=0; i<E; i++) {
+        int u,v,w;
+        cin >> u >> v >> w;
+        G[u].push_back(make_pair(w,v));
+        G[v].push_back(make_pair(w,u));
+    }
+    
+    Solution solve;
+    solve.djikstra(G, 0, dis, vis);
+    for(int i=0; i<V; i++) {
+        cout << i << " " << dis[i] << "\n";
+    }
+    cout << "\n";
+    return 0;
 }
